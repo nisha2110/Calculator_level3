fde6ea51522d082c121bd5a012bf83994044d393
nisha2110 <np857@njit.edu>
2024-09-25 15:23:54 -0700

    The calculation was restructured to go into a separate file

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,20 @@
-""" Basic arithmetic operations such as addition and subtraction."""
-def addition1(a,b):
-    """return sum of a and b"""
-    return a + b
+from calculator.calculation import Calculation
+from calculator.operations import addition1, subtract1, multiply, divide
 
-def subtract1(a,b):
-    """return subtract of a and b"""
-    return a - b
+class Calculator:
+    @staticmethod
+    def addition1(a,b):
+        calculation = Calculation(a, b, addition1)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract1(a,b):
+        calculation = Calculation(a, b, subtract1)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply(a,b):
+        calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
+        return calculation.get_result()
---
+++ calculator/operations.py
@@ -0,0 +1,16 @@
+""" Basic arithmetic operations."""
+def addition1(a,b):
+    """return sum of a and b"""
+    return a + b
+
+def subtract1(a,b):
+    """return subtract of a and b"""
+    return a - b
+
+def multiply(a,b):
+     """return mul of a and b"""
+     return a * b
+
+def divide(a,b):
+     """return div of a and b"""
+     return a / b 
